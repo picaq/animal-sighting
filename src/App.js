a372ae275ec3dc7f69539176bf8c728503944a3f
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-22 23:27:17 -0700

    create SightingsList list and NewSightings form components

--- src/App.js
+++ src/App.js
@@ -8,12 +8,48 @@ function App() {
         <h1> <img src={logo} className="App-logo" alt="logo" /> Animal Sighting Tracker </h1>
         <p>track sightings of endangered animals</p>
       </header>
+      
+      <main>
+        <SightingsList title="All Sightings" />
+        <NewSighting title="New Sighting"/>
 
+      </main>
       
     </div>
   );
 }
 
+const NewSighting = (props) => {
+  return(
+    <>
+      <h2>{props.title}</h2>
+      <form>
+        <label htmlFor="time seen">time seen
+          <input 
+            type="datetime" 
+            name="time seen" 
+            id="timeSeen"/>
+        </label>
+
+        <label htmlFor="nickname">nickname
+          <input 
+            type="text" 
+            name="nickname" 
+            id="nickname"/>
+        </label>
+      </form>
+    </>
+  )
+}
+
+const SightingsList = (props) => {
+  return(
+    <>
+      <h2>{props.title}</h2>
+      <ul></ul>
+    </>
+  )
+}
 
 
 
