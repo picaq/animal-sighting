f34b2f994f339d4cd826447ec0670f0f95a2b6c5
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-22 22:03:51 -0700

    add tagline, negative leading and @media

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> <img src={logo} className="App-logo" alt="logo" /> Animal Sighting Tracker </h1>
-
+        <p>track sightings of endangered animals</p>
       </header>
+
+      
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
 
 export default App;
