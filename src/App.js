20bf97efbf6c0cc47a9dd18f96366e049b7f1f23
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-22 21:44:22 -0700

    specify fonts Pangolin and Lato

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1> <img src={logo} className="App-logo" alt="logo" /> Animal Sighting Tracker </h1>
 
       </header>
     </div>
