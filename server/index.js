9c3302e71dd32ae77e6acb178f1ae0539851b97b
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-24 16:25:40 -0700

    start server on port 9000, require cors

--- server/index.js
+++ server/index.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.listen(9000, () => {
+    console.log("server has started on port 9000")
+})
