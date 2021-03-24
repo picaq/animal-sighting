8be5732bbd9b0a491e8c00e036e7023f25305e3b
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-24 16:26:48 -0700

    use express.json(), add ;

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+// gives access to request body to get json data
+app.use(express.json());
 
 app.listen(9000, () => {
     console.log("server has started on port 9000")
-})
+});
