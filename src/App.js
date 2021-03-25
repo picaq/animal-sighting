fc70ade3d7ee55350de04d7af3bb3eb1790011a2
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-25 10:24:36 -0700

    move NewSighting component to NewSighting.js in src dir; healthy checkbox onClick to onChange to fix React warning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import logo from './assets/paw.png';
 import React, { useState, useEffect } from 'react';
+import NewSighting from './NewSighting';
 import './App.css';
-
-let timeOnLoad = new Date().toISOString().split(/[T.]/);
-let today = timeOnLoad[0];
-let now = timeOnLoad[1];
 
 function App() {
 
@@ -24,182 +21,182 @@ function App() {
   );
 }
 
-const NewSighting = (props) => {
+// const NewSighting = (props) => {
 
-  const [dateSeen, setDateSeen] = useState(today);
-  useEffect( () => {
-    console.log("dateSeen changed", {dateSeen})
-  }, [dateSeen] );
+//   const [dateSeen, setDateSeen] = useState(today);
+//   useEffect( () => {
+//     console.log("dateSeen changed", {dateSeen})
+//   }, [dateSeen] );
 
-  const [timeSeen, setTimeSeen] = useState(now);
-  useEffect( () => {
-    console.log("timeSeen changed", {timeSeen})
-  }, [timeSeen] );
+//   const [timeSeen, setTimeSeen] = useState(now);
+//   useEffect( () => {
+//     console.log("timeSeen changed", {timeSeen})
+//   }, [timeSeen] );
 
-  const [nickname, setNickname] = useState("unnamed");
-  useEffect( () => {
-    console.log("nickname changed", {nickname})
-  }, [nickname] );
+//   const [nickname, setNickname] = useState("unnamed");
+//   useEffect( () => {
+//     console.log("nickname changed", {nickname})
+//   }, [nickname] );
 
-  const [individual_id, setID] = useState("");
-  useEffect( () => {
-    console.log("individual_id changed", {individual_id})
-  }, [individual_id] );
+//   const [individual_id, setID] = useState("");
+//   useEffect( () => {
+//     console.log("individual_id changed", {individual_id})
+//   }, [individual_id] );
 
-  const [location, setLocation] = useState("null island");
-  useEffect( () => {
-    console.log("email changed", {location})
-  }, [location] );
+//   const [location, setLocation] = useState("null island");
+//   useEffect( () => {
+//     console.log("email changed", {location})
+//   }, [location] );
 
-  const [healthy, setHealth] = useState(true);
-  useEffect( () => {
-    console.log("healthy changed", {healthy})
-  }, [healthy] );
+//   const [healthy, setHealth] = useState(true);
+//   useEffect( () => {
+//     console.log("healthy changed", {healthy})
+//   }, [healthy] );
 
-  const [email, setEmail] = useState("soylent@gmail.com");
-  useEffect( () => {
-    console.log("email changed", {email})
-  }, [email] );
+//   const [email, setEmail] = useState("soylent@gmail.com");
+//   useEffect( () => {
+//     console.log("email changed", {email})
+//   }, [email] );
 
-  const [species, setSpecies] = useState("unidentified");
-  useEffect( () => {
-    console.log("species changed", {species})
-  }, [species] );
+//   const [species, setSpecies] = useState("unidentified");
+//   useEffect( () => {
+//     console.log("species changed", {species})
+//   }, [species] );
 
 
   
-  let sendSightingObject = () => {
-    let timeStamp = new Date().toISOString();
-    let sightingObject = { dateSeen, timeSeen, species, nickname, individual_id, location, healthy, email, timeStamp };
-    console.log("Add Sighting button pressed", sightingObject);
-  }
+//   let sendSightingObject = () => {
+//     let timeStamp = new Date().toISOString();
+//     let sightingObject = { dateSeen, timeSeen, species, nickname, individual_id, location, healthy, email, timeStamp };
+//     console.log("Add Sighting button pressed", sightingObject);
+//   }
 
-  return(
-    <>
-      <h2>{ props.title }</h2>
-      <div>
-        <form>
-          <label htmlFor="new">
-            <input 
-              type="radio" 
-              id="new" 
-              name="which" 
-              value="new" /> 
-              New { props.name }
-          </label>
+//   return(
+//     <>
+//       <h2>{ props.title }</h2>
+//       <div>
+//         <form>
+//           <label htmlFor="new">
+//             <input 
+//               type="radio" 
+//               id="new" 
+//               name="which" 
+//               value="new" /> 
+//               New { props.name }
+//           </label>
 
-          <label htmlFor="existing">
-            <input 
-              type="radio" 
-              id="existing" 
-              name="which" 
-              value="existing" />
-              Existing { props.name }
-          </label>
-        </form>
-        <label htmlFor="date seen">date seen
-          <input 
-            type="date" 
-            name="date seen" 
-            id="dateSeen"
-            value={ dateSeen }
-            max={ today }
-            onChange={ (e) => setDateSeen(e.target.value) }  
-            />
-        </label>
+//           <label htmlFor="existing">
+//             <input 
+//               type="radio" 
+//               id="existing" 
+//               name="which" 
+//               value="existing" />
+//               Existing { props.name }
+//           </label>
+//         </form>
+//         <label htmlFor="date seen">date seen
+//           <input 
+//             type="date" 
+//             name="date seen" 
+//             id="dateSeen"
+//             value={ dateSeen }
+//             max={ today }
+//             onChange={ (e) => setDateSeen(e.target.value) }  
+//             />
+//         </label>
 
-        <label htmlFor="time seen">time seen
-          <input 
-            type="time" 
-            name="time seen" 
-            id="timeSeen"
-            value={ timeSeen }
-            onChange={ (e) => setTimeSeen(e.target.value) } 
-          />
-        </label>
+//         <label htmlFor="time seen">time seen
+//           <input 
+//             type="time" 
+//             name="time seen" 
+//             id="timeSeen"
+//             value={ timeSeen }
+//             onChange={ (e) => setTimeSeen(e.target.value) } 
+//           />
+//         </label>
 
-        <label htmlFor="nickname">
-          nickname
-          <input 
-            type="text" 
-            name="nickname" 
-            id="nickname"
-            // value={ nickname }
-            placeholder="an identifying name"
-            onChange={ (e) => setNickname(e.target.value) }   
-            />
-        </label>
+//         <label htmlFor="nickname">
+//           nickname
+//           <input 
+//             type="text" 
+//             name="nickname" 
+//             id="nickname"
+//             // value={ nickname }
+//             placeholder="an identifying name"
+//             onChange={ (e) => setNickname(e.target.value) }   
+//             />
+//         </label>
         
-        <label htmlFor="species">
-        species
-          <input 
-            type="text" 
-            name="species" 
-            id="species"
-            placeholder="scientific binomial"
-            onChange={ (e) => setSpecies(e.target.value) }   
-            />
-        </label>        
+//         <label htmlFor="species">
+//         species
+//           <input 
+//             type="text" 
+//             name="species" 
+//             id="species"
+//             placeholder="scientific binomial"
+//             onChange={ (e) => setSpecies(e.target.value) }   
+//             />
+//         </label>        
 
-        <label htmlFor="id">
-          id
-          <input 
-            type="number" 
-            name="id"
-            min="0" 
-            id="id"
-            value={ individual_id }
-            placeholder="individual id #"
-            onChange={ (e) => setID(e.target.value) } 
-            />
-        </label>
+//         <label htmlFor="id">
+//           id
+//           <input 
+//             type="number" 
+//             name="id"
+//             min="0" 
+//             id="id"
+//             value={ individual_id }
+//             placeholder="individual id #"
+//             onChange={ (e) => setID(e.target.value) } 
+//             />
+//         </label>
 
-        <label htmlFor="location">
-          location
-          <input 
-            type="text" 
-            name="location" 
-            id="location"
-            placeholder="my backyard"
-            onChange={ (e) => setLocation(e.target.value) } 
-            />    
-        </label>
+//         <label htmlFor="location">
+//           location
+//           <input 
+//             type="text" 
+//             name="location" 
+//             id="location"
+//             placeholder="my backyard"
+//             onChange={ (e) => setLocation(e.target.value) } 
+//             />    
+//         </label>
 
-        <label htmlFor="healthy">
-          healthy
-          <input 
-            type="checkbox" 
-            name="healthy" 
-            id="healthy"
-            checked= { healthy }
-            onClick={ () => setHealth( healthy ? false : true ) } 
-            />
-        </label>
+//         <label htmlFor="healthy">
+//           healthy
+//           <input 
+//             type="checkbox" 
+//             name="healthy" 
+//             id="healthy"
+//             checked= { healthy }
+//             onClick={ () => setHealth( healthy ? false : true ) } 
+//             />
+//         </label>
 
-        <label htmlFor="email">
-          email
-          <input 
-            type="text" 
-            name="email" 
-            id="email"
-            placeholder={ email }
-            onChange={ (e) => setEmail(e.target.value) } 
-            />
-        </label>
+//         <label htmlFor="email">
+//           email
+//           <input 
+//             type="text" 
+//             name="email" 
+//             id="email"
+//             placeholder={ email }
+//             onChange={ (e) => setEmail(e.target.value) } 
+//             />
+//         </label>
 
-      <button
-      /* type="submit"  */
-        onClick={ () => sendSightingObject() }
-        name={ props.title }
-        value={ props.title }
-      > 
-      Add { props.title }
-      </button>
+//       <button
+//       /* type="submit"  */
+//         onClick={ () => sendSightingObject() }
+//         name={ props.title }
+//         value={ props.title }
+//       > 
+//       Add { props.title }
+//       </button>
 
-      </div>
-    </>
-  )
-}
+//       </div>
+//     </>
+//   )
+// }
 
 const SightingsList = (props) => {
   return(
